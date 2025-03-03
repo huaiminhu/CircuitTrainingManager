7b89f8f6cdf0254150467d810f0f7e3337654761
Hank Hu <hankhuwm@gmail.com>
2025-03-03 21:58:44 +0800

    1st commit

---
+++ DataAccess/Entities/Circuit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    public class Circuit
+    {
+        public int CircuitId { get; set; }
+        public int UserId { get; set; }
+        public required User User { get; set; }
+        public int GoalId { get; set; }
+        public required Goal Goal { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public required List<Exercise> Exercises { get; set; }
+        public int StartValue { get; set; }
+        public int ResultValue { get; set; }
+        public int AchievementRate { get; set; }
+        public required string Note { get; set; }
+    }
+}
---
+++ DataAccess/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public required string NickName { get; set; }
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+        public List<Circuit> Circuits { get; set; }
+    }
+}
